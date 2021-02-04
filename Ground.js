b4d748b44007f9e8681ee69d0befe87edd34f7a7
Barbz1 <samonak94@gmail.com>
2021-02-03 18:48:36 -0700

    platform changed

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill("SaddleBrown");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
